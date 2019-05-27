febfd5860150f5c38fdca89a7ecb5b25aad3c08c
Panupong Pasupat <ppasupat@stanford.edu>
2019-05-27 00:37:16 -0700

    Updated to Chaos Impact

--- parse.py
+++ parse.py
@@ -2,10 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, shutil, re, argparse, json
-from codecs import open
-from itertools import izip
 from collections import defaultdict, Counter
-
 
 
 def main():
@@ -17,18 +14,18 @@ def main():
     
     entries = []
     for filename in args.infiles:
-        with open(filename, 'r', 'utf8') as fin:
+        with open(filename) as fin:
             data = json.load(fin)
             for x in data['results'].values():
                 printouts = x['printouts']
-                entries.append({key: (value[0] if value else '0') for (key, value) in printouts.iteritems()})
+                entries.append({key: (value[0] if value else '0') for (key, value) in printouts.items()})
     entries.sort(key=lambda x: x['English name'])
 
-    with open(args.outfile, 'w', 'utf8') as fout:
+    with open(args.outfile, 'w') as fout:
         fout.write('var ' + args.name + ' = ')
         json.dump(entries, fout, separators=(',', ':'), ensure_ascii=False)
         fout.write(';\n')
-    print >> sys.stderr, 'Wrote {} entries'.format(len(entries))
+    print('Wrote {} entries'.format(len(entries)), file=sys.stderr)
 
 
 if __name__ == '__main__':
